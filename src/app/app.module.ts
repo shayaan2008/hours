f8e6fd9d4c15784573f436d194f39c47199f0a9c
shayaan2008 <67163498+shayaan2008@users.noreply.github.com>
2021-03-17 13:54:20 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HoursComponent } from './component/hours/hours.component';
+import { ReviewsComponent } from './component/reviews/reviews.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HoursComponent,
+    ReviewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
